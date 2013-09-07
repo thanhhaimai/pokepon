dc49c9d9735ccd97cc87b88e7ccb2210e8559b96
Melanie Cebula <melaniecebula@berkeley.edu>
2013-09-07 02:18:29 -0400

    badmerge fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var routes = require('./routes');
-var game = require('./routes/game');
+var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -30,9 +30,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', game.list);
-app.get('/game', game.list);
-app.get('/game/create', game.create);
+app.get('/', routes.index);
+app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
