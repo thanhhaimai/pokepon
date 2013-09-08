b9103036c9bb71751dba55f9dbbcf8d5f32fe540
Achal Dave <achalddave@live.com>
2013-09-08 04:58:59 +0000

    change client.js to use new html classes/ids

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -45,9 +45,9 @@ Client.prototype.connect = function() {
       var pokemon = snapshot.val();
       var firstNumber = pokemon.urlNumber;
       var secondNumber = pokemon.urlNumber;
-      var firstPokemon = $('#youpokemon')[0];
+      var firstPokemon = $('#you .pokemon')[0];
       firstPokemon.src = 'http://sprites.pokecheck.org/i/' + firstNumber + '.gif';
-      var secondPokemon = $('#opponentpokemon')[0];
+      var secondPokemon = $('#opponent .pokemon')[0];
       secondPokemon.src = 'httpL//sprites.pokecheck.org/i/' + secondNumber + '.gif';
     });
     // this is the my pokepon data.
