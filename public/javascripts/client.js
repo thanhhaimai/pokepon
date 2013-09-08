069f123de419ea35075f6bfb61f018b380b735ab
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-09-07 19:46:20 -0700

    * fix build

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -10,7 +10,6 @@ Client.prototype.connect = function() {
   self.gameId = url.substr(url.lastIndexOf('/') + 1);
 
   self.socket = io.connect('http://localhost:3000');
-<<<<<<< HEAD
 
   self.socket.on('joined', function(player) {
     self.id = player.id;
@@ -26,8 +25,6 @@ Client.prototype.connect = function() {
     if (data.gameId !== self.gameId) {
       return;
     }
-=======
->>>>>>> making the distance
 
     self.isStarted = true;
 
